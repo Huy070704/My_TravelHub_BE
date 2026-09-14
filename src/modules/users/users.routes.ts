6b9def5338f4318c29bcd3259bba422ad828f795
Huy070704 <he187068maithanhhuy@gmail.com>
2026-09-14 15:37:30 +0700

    /get/me

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { UsersController } from './users.controller';
+import { authenticateToken } from '../../middleware/auth.middleware';
 
 const router = Router();
 
+router.get('/me', authenticateToken, UsersController.getMe);
 router.get('/', UsersController.getAll);
 
 export { router as userRoutes };
